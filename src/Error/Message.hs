dadaaf4561438f545a1fb9343fbc260534970b23
Felipe g <chiyokuoni@gmail.com>
2022-03-19 20:57:52 -0300

    refactor: error messages are better now

--- src/Error/Message.hs
+++ src/Error/Message.hs
@@ -1,11 +1,9 @@
 module Error.Message where
 
-import Data.Text       (Text)
-import Syntax.Bounds   (Bounds, Pos, WithBounds)
-
+import Data.Text (Text)
+import Syntax.Bounds (Bounds, Pos, WithBounds)
 import qualified Syntax.Bounds as B
 import Syntax.Lexer.Tokens (Token (TknEOF))
-import qualified Data.Text as Text
 
 data ErrorKind
   = UnfinishedString Pos
@@ -38,7 +36,7 @@ onlyCol :: Pos -> Bounds
 onlyCol pos@(B.Pos line col) = B.Bounds pos (B.Pos line (col + 1))
 
 columnError :: Pos -> Text -> ErrMessage
-columnError pos text = ErrMessage (Just pos) text [Code (onlyCol pos) Nothing]
+columnError pos text = ErrMessage (Just pos) text [Code (onlyCol pos) (Just "Here!!")]
 
 boundsError :: Bounds -> Text -> ErrMessage
 boundsError pos text = ErrMessage (Just $ B.start pos) text [Code pos (Just "Here!")]
@@ -46,8 +44,6 @@ boundsError pos text = ErrMessage (Just $ B.start pos) text [Code pos (Just "Her
 messageFromErr :: ErrorKind -> ErrMessage
 messageFromErr (UnexpectedToken (B.WithBounds TknEOF pos)) = boundsError pos "Unexpected end of file! "
 messageFromErr (UnexpectedToken (B.WithBounds _ pos)) = boundsError pos "Cannot uwndustwand this tUwUken"
-
-
 messageFromErr (UnfinishedString pos) = columnError pos "Probably you forgot to close a quote while trying to create a string!"
 messageFromErr (UnrecognizableChar pos) = columnError pos "Cannot understand this character bro UwU"
 messageFromErr (UnexpectedAssign pos) = boundsError pos "You cant use let expressions in the end of a block"
--- src/Error/PrettyPrint.hs
+++ src/Error/PrettyPrint.hs
@@ -1,62 +1,78 @@
 module Error.PrettyPrint where
 
 import Data.Text (Text)
-import Error.Message (ErrReport)
-import Syntax.Bounds (Bounds, Pos)
-
-import System.Console.Pretty ( Color(Cyan, Red), Pretty(color, bgColor, style), Style(Faint, Bold) )
-import Text.Printf ( printf )
-
-import qualified Syntax.Bounds as B
-import qualified Error.Message as M
 import qualified Data.Text as T
-
-trimLines :: [(Int, a)] -> [(Int, a)]
-trimLines []    = []
-trimLines [x]   = [x]
-trimLines other = [head other, last other]
-
-getLines :: Bounds -> Text -> [(Int, Text)]
-getLines bounds text =
-        let lines' = T.lines text
-            start  = B.line (B.start bounds)
-            len    = max (start - B.line (B.end bounds)) 1
-        in trimLines $
-            zip (map (+ (start + 1)) [0..len])
-                (take len (drop start lines'))
-
-badge :: Text -> Color -> Text
-badge kind color' = bgColor color' (T.concat [" ", kind ," "])
-
-showLine :: Text -> Text
-showLine title = T.pack $ printf " %s %s" (badge "ERROR" Red) title
-
-showCode :: Text -> Maybe Pos -> Text
-showCode file (Just pos) = T.pack $ printf "%s:%d:%d" file (B.line pos + 1) (B.column pos)
-showCode _ Nothing = ""
-
-showCodeLine :: (Int, Text) -> Text
-showCodeLine (num, text) = T.pack $ printf "%4d  | %s" num text
+import qualified Error.Message as M
+import Syntax.Bounds (Bounds, Pos)
+import qualified Syntax.Bounds as B
+import System.Console.Pretty (Color (Red), Pretty (bgColor, color, style), Style (Faint))
+import qualified Text.Printf as P
 
 space :: Int -> Text
-space len = T.pack (replicate len ' ')
+space n = T.replicate n " "
 
-showTip :: Pos -> Text -> Text
-showTip pos text = T.pack $ style Bold
-                          $ color Red
-                          $ printf "%*s◮  %s" (B.column pos + 7) (" " :: String) text
+subString :: Int -> Int -> Text -> Text
+subString st_col end_col = T.take (end_col - st_col) . T.drop st_col
 
-ppShow :: ErrReport -> Text
-ppShow (M.ErrReport file content kind) =
-    let message = M.messageFromErr kind in
-    "\n" <> T.intercalate "\n\n"
-                [ showLine (M.errTitle message)
-                , T.concat ["   ", style Faint . color Cyan $ showCode file (M.errBounds message)]
-                , T.unlines (map ppShowMessage (M.errComponents message)) ]
-    where
-        ppShowMessage (M.Desc text) = T.concat [ "   ", text, "\n" ]
-        ppShowMessage (M.Code bounds anot) =
-                T.unlines $ map showCodeLine (getLines bounds content) ++ tip anot
-            where
-                tip (Just tip') = [showTip (B.start bounds) tip']
-                tip Nothing = []   
+splitText :: Int -> Int -> Text -> (Text, Text, Text)
+splitText start end text =
+  let startText = subString 0 start text
+      middleText = subString start end text
+      endText = subString end (T.length text) text
+   in (startText, middleText, endText)
+
+colorizeLine :: Color -> Int -> Int -> Text -> Text
+colorizeLine clr start end text =
+  let (startText, middleText, endText) = splitText (start - 1) (end - 1) text
+   in T.concat [startText, color clr middleText, endText]
+
+ppLine :: Int -> Text -> Text
+ppLine line = T.pack . P.printf "%5d | %s\n" line
+
+ppNote :: Color -> Int -> Int -> Maybe Text -> Text
+ppNote clr col len (Just text) = T.concat [space (col + 7), color clr (T.append "└" (T.replicate (len - 1) "─")), " ", color clr text]
+ppNote _ _ _ Nothing = ""
+
+-- I'll probably have to rewrite this lol. it's ugly as fuck
+ppLines :: Maybe Text -> Color -> Text -> Bounds -> Text
+ppLines note clr source (B.Bounds start end)
+  | B.line start == B.line end =
+    let line = T.lines source !! B.line start
+     in T.concat
+          [ ppLine' (B.line start) (B.column start) (B.column end) line,
+            ppNote clr (B.column start) (B.column end - B.column start) note
+          ]
+  | otherwise =
+    let lines' = T.lines source
+        line = lines' !! B.line start
+        line' = lines' !! B.line end
+        fstLine = ppLine' (B.line start) (B.column start) (T.length line) line
+        sndLine = ppLine' (B.line end) 0 (B.column end) line'
+     in T.unlines [fstLine, ppNote clr (B.column start) (T.length line - B.column start) note, sndLine]
+  where
+    ppLine' line st en = ppLine line . colorizeLine clr st en
+
+ppFile :: Text -> Maybe Pos -> Text
+ppFile text (Just (B.Pos line col)) = T.pack $ P.printf "%s --> %s:%d:%d\n\n" (space 4) text line col
+ppFile text Nothing = T.pack $ P.printf "%s --> %s\n\n" (space 4) text
+
+ppTitle :: Text -> Text
+ppTitle t = T.concat [space 4, bgColor Red " ERROR ", " ", t, "\n"]
+
+ppComponent :: Text -> M.ErrComponent -> Text
+ppComponent source (M.Code bounds note) = T.append (ppLines note Red source bounds) "\n"
+ppComponent _ (M.Desc t) = T.concat [space 4, t, "\n\n"]
+
+ppErrorMessage :: Text -> Text -> M.ErrMessage -> Text
+ppErrorMessage source subLine (M.ErrMessage _ title components) =
+  T.concat $
+    [ "\n",
+      ppTitle title,
+      style Faint subLine
+    ]
+      ++ map (ppComponent source) components
+
+ppErrorReport :: M.ErrReport -> Text
+ppErrorReport (M.ErrReport file content kind) =
+  let beKind = M.messageFromErr kind
+   in ppErrorMessage content (ppFile file (M.errBounds beKind)) beKind
