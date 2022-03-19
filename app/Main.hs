f1e385fa1368b586c8c44771beec07c5783a9b94
Felipe g <chiyokuoni@gmail.com>
2022-03-19 20:58:21 -0300

    test: fixed tests

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,23 @@
 module Main where
 
-import GHC.IO.Encoding ( utf8, setLocaleEncoding )
-import Syntax.Lexer.Support ( runLexer )
-import Syntax.Parser ( parseExpr )
-import System.Environment ( getArgs )
-
-import Data.Text.Encoding (decodeUtf8)
-import Data.Text (pack, unpack)
-
-import Error.Message ( ErrReport(ErrReport) )
-import Error.PrettyPrint ( ppShow )
-import Pretty.Tree ( drawTree )
-
 import qualified Data.ByteString as SB
+import Data.Text (pack, unpack)
+import Data.Text.Encoding (decodeUtf8)
+import GHC.IO.Encoding (setLocaleEncoding, utf8)
+import Pretty.Tree (drawTree)
+import System.Environment (getArgs)
+
+import Error.Message (ErrReport (ErrReport))
+import Error.PrettyPrint (ppErrorReport)
+
+import Syntax.Lexer.Support (runLexer)
+import Syntax.Parser (parseProgram)
 
 main :: IO ()
 main = do
   setLocaleEncoding utf8
   [file] <- getArgs
-  str    <- SB.readFile file
-  case runLexer parseExpr str of
-      Right res -> do
-        putStrLn $ drawTree res
-      Left err -> putStrLn (unpack $ ppShow $ ErrReport (pack file) (decodeUtf8 str) err)
+  str <- SB.readFile file
+  case runLexer parseProgram str of
+    Right res -> putStrLn $ drawTree res
+    Left err -> putStrLn (unpack $ ppErrorReport $ ErrReport (pack file) (decodeUtf8 str) err)
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,17 +10,26 @@ import Syntax.Lexer.Support
 import Syntax.Parser
 
 import qualified Data.ByteString as SB
+import qualified Data.Text as Text
+
+import Error.PrettyPrint (ppErrorReport)
+import Error.Message (ErrReport(ErrReport))
+import Data.Text.Encoding (decodeUtf8)
+import GHC.IO.Exception (assertError)
+
+excp :: SB.ByteString -> FilePath -> IO ()
+excp content file =
+  case runLexer parseProgram content of
+    Right _ -> pure ()
+    Left err -> error ("\n" ++ Text.unpack (ppErrorReport $ ErrReport (Text.pack file) (decodeUtf8 content) err))
 
 runFile :: FilePath -> IO TestTree
-runFile file = do 
+runFile file = do
   content <- SB.readFile $ addExtension file ".nk"
-  pure $ testCase ("Parsing '" ++ file ++ "'") $
-     assertEqual ("The file '" ++ file ++ "' will be parsed!")
-               True
-               (isRight (runLexer parseProgram content))
- 
+  pure $ testCase ("Parsing '" ++ file ++ "'") $ assertError True (excp content file)
+
 main :: IO ()
-main = do 
+main = do
     filesNoExt <- map dropExtension <$> findByExtension [".nk"] "test/suite"
     tests <- for filesNoExt runFile
     defaultMain $ testGroup "Parsing files" tests
