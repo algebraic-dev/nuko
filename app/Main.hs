b047b17e93ad1c1647a1c4b1afb868c642d8da90
Felipe g <chiyokuoni@gmail.com>
2022-04-16 16:02:59 -0300

    feat: changed to GHC 9.2.2

--- app/Main.hs
+++ app/Main.hs
@@ -9,16 +9,18 @@ import System.Environment (getArgs)
 import Error.PrettyPrint (ppErrorReport)
 import Error.Message (ErrReport (ErrReport))
 
-import Syntax.Parser ( parseType ) 
+import Syntax.Parser ( parseProgram ) 
 import Syntax.Lexer.Support (runLexer)
 
 import qualified Data.ByteString as SB
+import Syntax.Lexer (scan)
+import Control.Monad (replicateM)
 
 main :: IO ()
 main = do
   setLocaleEncoding utf8
   [file] <- getArgs
   str <- SB.readFile file
-  case runLexer parseType str of
-    Right res -> putStrLn (drawTree res)
+  case runLexer (replicateM 10 scan) str of
+    Right res -> putStrLn (show res)
     Left err -> putStrLn (unpack $ ppErrorReport $ ErrReport (pack file) (decodeUtf8 str) err) 
