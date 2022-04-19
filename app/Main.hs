2e7e9c81696354760de37dc562bd20d450703a3b
Felipe g <chiyokuoni@gmail.com>
2022-04-19 14:01:59 -0300

    feat: added transformers

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,43 @@
 module Main (main) where
 
-import Data.Text (pack, unpack, Text)
-import Data.Text.Encoding (decodeUtf8)
-import GHC.IO.Encoding (setLocaleEncoding, utf8)
-import System.Environment (getArgs)
-import Error.PrettyPrint (ppErrorReport)
-import Error.Message (ErrReport (ErrReport))
+import Data.Text            (pack, unpack, Text)
+import Data.Text.Encoding   (decodeUtf8)
+import GHC.IO.Encoding      (setLocaleEncoding, utf8)
+import System.Environment   (getArgs)
+import Error.PrettyPrint    (ppErrorReport)
+import Error.Message        (ErrReport (ErrReport))
 import Syntax.Lexer.Support (runLexer)
-import Typer.Infer (inferExpr)
-import Typer.Env (Env(Env), runEnv, Ty (TyForall, TyNamed, TyFun))
-import Typer.Error (TypeError)
-import Control.Exception (catch)
-import Data.Map (Map)
-import Syntax.Range (Loc(Blank))
-import Syntax.Parser (parseExpr)
+import Typer.Infer          (inferExpr)
+import Typer.Env            (Env(Env), runEnv, Ty (TyForall, TyNamed, TyFun))
+import Typer.Error          (TypeError)
+import Control.Exception    (catch)
+import Data.Map             (Map)
+import Syntax.Parser        (parseExpr)
 
 import qualified Data.ByteString as SB
-import qualified Data.Map as Map
+import qualified Data.Map        as Map
+import qualified Data.Sequence   as Seq
+import qualified Data.Set        as Set
+
+infixr 3 ~>
+
+v :: Text -> Ty
+v = TyNamed Nothing
+
+(~>) :: Ty -> Ty -> Ty
+(~>) = TyFun Nothing
+
+forall' :: Text -> Ty -> Ty
+forall' = TyForall Nothing
 
 vars :: Map Text Ty
 vars = Map.fromList
   [
-    ("k", TyForall Blank "b" (TyFun Blank (TyForall Blank "a" (TyNamed Blank "a")) (TyNamed Blank "b")))
-  , ("f1", TyFun Blank ( (TyFun Blank (TyNamed Blank "Int") (TyFun Blank (TyNamed Blank "Int") (TyNamed Blank "Int")))) (TyNamed Blank "Int"))
-  , ("f2", TyFun Blank (TyForall Blank "a" (TyFun Blank (TyNamed Blank "a") (TyFun Blank (TyNamed Blank "a") (TyNamed Blank "a")))) (TyNamed Blank "Int"))
-
+    ("k",  forall' "b" (forall' "a" (v "a")) ~> (v "b"))
+  , ("f1", (v "Int" ~> v "Int" ~> v "Int") ~> v "Int")
+  , ("f2", (forall' "a" ((v "a") ~> v "a" ~> v "a")) ~> v "Int")
+  , ("g", forall' "b" (forall' "a" (v "Int")) ~> (v "Int"))
+  , ("g1", forall' "b" (forall' "a" (v "Bool")))
   ]
 
 main :: IO ()
@@ -34,6 +47,6 @@ main = do
   str <- SB.readFile file
   case runLexer parseExpr str  of
     Right res -> do
-      catch (runEnv (inferExpr res) (Env 0 0 [] vars mempty) >>= print)
+      catch (runEnv (inferExpr res) (Env 0 0 Seq.empty vars (Set.fromList ["Int", "Bool"])) >>= print)
             ((\err -> putStrLn (unpack $ ppErrorReport $ ErrReport (pack file) (decodeUtf8 str) err)) :: TypeError -> IO ())
     Left err -> putStrLn (unpack $ ppErrorReport $ ErrReport (pack file) (decodeUtf8 str) err)
