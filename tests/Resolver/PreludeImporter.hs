f12b1cf1fb0d6ff6ad0d176a07c2f1f67fb972cf
Felipe g <chiyokuoni@gmail.com>
2022-07-02 19:20:00 -0300

    tests: added tests

---
+++ tests/Resolver/PreludeImporter.hs
@@ -0,0 +1,56 @@
+module Resolver.PreludeImporter where
+
+
+import Nuko.Resolver.Environment (NameSpace, LocalNS(..), Label(..), emptyLocalNS)
+import Nuko.Resolver.Occourence  (NameKind(..), OccName(..), insertEnv)
+import Nuko.Resolver.Error       (ResolveError)
+import Nuko.Resolver.Types       (MonadResolver)
+import Control.Monad.Import      (MonadImport (importIn, addIn), ImportErrorKind (CannotFind))
+import Control.Monad.Chronicle   (MonadChronicle(..))
+import Relude.Monad              (Monad ((>>=)), MonadState (..))
+import Relude.Monoid             (Endo(appEndo))
+import Relude.Functor            (Functor, fmap, (<$>), first)
+import Relude.Applicative        (Applicative(pure, (<*>)))
+import Relude                    (($), Either (..), (.))
+import Data.These                (These)
+
+import qualified Control.Monad.State as State
+import qualified Control.Monad.Chronicle as Chronicle
+import qualified Nuko.Resolver.Occourence as Occ
+
+data TestImport m s = TestImport { runNull :: m s }
+
+instance Monad m => Functor (TestImport m) where fmap f (TestImport s) = (TestImport (f <$> s))
+
+instance Monad m => Applicative (TestImport m) where
+  pure s    = TestImport (pure s)
+  (<*>) a b = TestImport ((runNull a) <*> (runNull b))
+
+instance Monad m => Monad (TestImport m) where (>>=) b a = TestImport ((a <$> (runNull b)) >>= runNull)
+
+instance Monad m => MonadImport NameSpace (TestImport m) where
+  importIn _ = TestImport (pure $ Left CannotFind)
+  addIn _ _  = pure ()
+
+instance MonadState s m => MonadState s (TestImport m) where
+  state = TestImport . state
+
+instance MonadChronicle s m => MonadChronicle s (TestImport m) where
+  dictate     = TestImport . dictate
+  confess     = TestImport . confess
+  absolve a f = TestImport (absolve a (runNull f))
+  retcon  s f = TestImport (retcon s (runNull f))
+  memento f   = TestImport $ memento (runNull f)
+  condemn f   = TestImport $ condemn (runNull f)
+  chronicle f = TestImport $ chronicle f
+
+
+runResolverNull :: (forall m . MonadResolver m => m a) -> These [ResolveError] (a, LocalNS)
+runResolverNull action =
+    let localNS  = (emptyLocalNS "Main") { _openedNames = openedLs}
+        openedLs = insertEnv (OccName "Int" TyName)    (Single "Int" "Prelude")
+                 $ insertEnv (OccName "String" TyName) (Single "String" "Prelude")
+                 $ Occ.empty
+    in first
+       (`appEndo` [])
+       (Chronicle.runChronicle $ State.runStateT (runNull action) localNS)
--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,22 +1,20 @@
 import Relude
-
-import Relude.String (unlines)
-import Relude.Base
 import Data.These
 
 import Test.Tasty
-import Test.Tasty.Golden         (findByExtension, goldenVsString)
-import System.FilePath           (dropExtension, addExtension)
+import Test.Tasty.Golden             (findByExtension, goldenVsString)
+import System.FilePath               (dropExtension, addExtension)
 
-import Nuko.Syntax.Lexer.Support
-import Nuko.Syntax.Lexer         (scan)
-import Nuko.Syntax.Lexer.Tokens  (Token(TcEOF))
-import Nuko.Syntax.Range         (Ranged (info))
-import Nuko.Syntax.Parser        (parseProgram)
-import Text.Pretty.Simple        (pShowNoColor)
+import Nuko.Resolver                 (resolveProgram)
+import Nuko.Syntax.Lexer.Support     (runLexer, Lexer)
+import Nuko.Syntax.Lexer             (scan)
+import Nuko.Syntax.Lexer.Tokens      (Token(TcEOF))
+import Nuko.Syntax.Range             (Ranged (info))
+import Nuko.Syntax.Parser            (parseProgram)
 
-import Relude.Foldable               (traverse)
-import Data.ByteString.Lazy.Internal (packChars)
+import Text.Pretty.Simple            (pShowNoColor)
+
+import Resolver.PreludeImporter      (runResolverNull)
 
 import qualified Data.ByteString as ByteString
 import qualified Data.Text.Lazy as LazyT
@@ -33,6 +31,11 @@ goldenStr file golden str = goldenVsString file golden (pure $ encodeUtf8 str)
 
 prettyShow :: Show a => a -> Text
 prettyShow item = LazyT.toStrict $ pShowNoColor item
+
+stringifyErr :: Show a => These [a] b -> These Text b
+stringifyErr (That e) = That e
+stringifyErr (This e) = This (unlines $ map prettyShow e)
+stringifyErr (These e f) = These (unlines $ map prettyShow e) f
 
 runFile :: FilePath -> IO TestTree
 runFile file = do
@@ -52,6 +55,15 @@ runParser file = do
     This e    -> goldenStr file golden ("This " <> unlines (map prettyShow e))
     These e f -> goldenStr file golden ("These " <> unlines (map prettyShow e) <> "\n" <> prettyShow f)
 
+runResolver :: FilePath -> IO TestTree
+runResolver file = do
+  content <- ByteString.readFile $ addExtension file ".nk"
+  let golden = addExtension file ".golden"
+  pure $ case stringifyErr (runLexer parseProgram content) >>= \x -> stringifyErr (runResolverNull (resolveProgram x)) of
+    That e    -> goldenStr file golden ("That " <> prettyShow e)
+    This e    -> goldenStr file golden ("This " <> e)
+    These e f -> goldenStr file golden ("These " <> e <> "\n" <> prettyShow f)
+
 runTestPath :: TestName -> FilePath -> (FilePath -> IO TestTree) -> IO TestTree
 runTestPath name path run = do
   filesNoExt <- map dropExtension <$> findByExtension [".nk"] path
@@ -62,5 +74,6 @@ main :: IO ()
 main = do
   testTree <- sequence
     [ runTestPath "Lexing" "tests/lexer" runFile
-    , runTestPath "Parsing" "tests/parser" runParser ]
+    , runTestPath "Parsing" "tests/parser" runParser
+    , runTestPath "Parsing" "tests/resolver" runResolver ]
   defaultMain $ Test.Tasty.testGroup "Tests" testTree
