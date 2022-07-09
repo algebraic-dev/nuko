121e5f0ec56194a398e72e71730016cba5e0d192
Felipe g <chiyokuoni@gmail.com>
2022-07-08 22:02:24 -0300

    tests: fix tests for differences in resolver

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,6 +1,6 @@
 
 
-import Relude                      (id, ($), Show, IO, unlines, FilePath, ConvertUtf8(encodeUtf8), ByteString, (.), fst, Bool (..), HashMap, zip)
+import Relude                      (id, ($), Show, IO, unlines, FilePath, ConvertUtf8(encodeUtf8), ByteString, (.), fst, Bool (..), HashMap, zip, Applicative ((<*)), uncurry)
 import Relude.String               (Text)
 import Relude.Monoid               (Semigroup((<>)),Endo(appEndo))
 import Relude.Monad                (Monad((>>=)), MonadIO)
@@ -19,9 +19,10 @@ import Nuko.Syntax.Lexer           (scan)
 import Nuko.Syntax.Lexer.Tokens    (Token(TcEOF))
 import Nuko.Syntax.Range           (Ranged (info))
 import Nuko.Syntax.Parser          (parseProgram)
-import Nuko.Typer.Env              (MonadTyper, TypeSpace, emptyTS)
-import Nuko.Typer.Infer            (initTypeDecl, inferTypeDecl)
+import Nuko.Typer.Env              (MonadTyper, TypeSpace(..), emptyTS, unifyHolesWithStar, TyInfo(..))
+import Nuko.Typer.Infer            (initTypeDecl, inferTypeDecl, checkTypeSymLoop)
 import Nuko.Typer.Error            (TypeError)
+import Nuko.Typer.Types            (TKind(..))
 
 import Text.Pretty.Simple          (pShowNoColor)
 
@@ -30,11 +31,12 @@ import Pretty.Tree                 (PrettyTree(prettyShowTree))
 
 import Nuko.Tree
 
-import qualified Data.ByteString as ByteString
-import qualified Data.Text.Lazy as LazyT
-import qualified Control.Monad.State.Strict as State
+import qualified Data.HashMap.Strict           as HashMap
+import qualified Data.ByteString               as ByteString
+import qualified Data.Text.Lazy                as LazyT
+import qualified Control.Monad.State.Strict    as State
 import qualified Control.Monad.Trans.Chronicle as Chronicle
-import qualified Control.Monad.Reader as Reader
+import qualified Control.Monad.Reader          as Reader
 
 scanUntilEnd :: Lexer [Ranged Token]
 scanUntilEnd = do
@@ -73,23 +75,25 @@ runParser = runThat prettyShowTree (pure . runLexer parseProgram)
 runResolver :: FilePath -> IO TestTree
 runResolver = runThat prettyShowTree $ \content -> pure (stringifyErr (runLexer parseProgram content) >>= \ast -> stringifyErr (resolveEntireProgram ast))
 
-runTypeChecker :: MonadIO m => (forall m . MonadTyper m => m a) -> m (These [TypeError] (a, TypeSpace))
-runTypeChecker act =
-  first (`appEndo` []) <$> (Chronicle.runChronicleT (Reader.runReaderT (State.runStateT act emptyTS) emptyTS))
+runTypeChecker :: MonadIO m => TypeSpace -> (forall m . MonadTyper m => m a) -> m (These [TypeError] (a, TypeSpace))
+runTypeChecker ts act =
+  first (`appEndo` []) <$> (Chronicle.runChronicleT (Reader.runReaderT (State.runStateT act emptyTS) ts))
 
 runTyper :: FilePath -> IO TestTree
 runTyper = runThat prettyShowTree $ \content -> do
     let result = stringifyErr (runLexer parseProgram content) >>= \ast -> stringifyErr (resolveEntireProgram ast)
+    let ts = emptyTS { _tsTypes = HashMap.fromList [("Prelude.Int", (KiStar, IsTyDef))] }
     case result of
       This a    -> pure (This (fmap prettyShow a))
-      That a    -> stringifyErr <$> runTypeChecker (run (fst a))
-      These b a -> stringifyErr <$> runTypeChecker (run (fst a))
+      That a    -> stringifyErr <$> runTypeChecker ts (run (fst a))
+      These b a -> stringifyErr <$> runTypeChecker  ts(run (fst a))
   where
     run :: MonadTyper m => Program Re -> m [TypeDecl Tc]
     run program = do
-      bindings <- traverse initTypeDecl program.typeDecls
-      forM (zip program.typeDecls bindings) $ \(decl, info) ->
-        inferTypeDecl decl info
+      initDatas <- traverse initTypeDecl program.typeDecls
+      checkTypeSymLoop program.typeDecls
+      checkedTc <- traverse (uncurry inferTypeDecl) (zip program.typeDecls initDatas)
+      pure checkedTc
 
 runTestPath :: TestName -> FilePath -> (FilePath -> IO TestTree) -> IO TestTree
 runTestPath name path run = do
